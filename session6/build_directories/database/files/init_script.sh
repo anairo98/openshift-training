833007f2b600b097c57a26d6aa44cfbb8550585d
christian <christian@christian-ThinkPad.home>
2025-09-11 23:50:39 +0200

    Session6 database conditional init_db and correct env for front and backend

--- session6/build_directories/database/files/init_script.sh
+++ session6/build_directories/database/files/init_script.sh
@@ -1,1 +1,4 @@
-mysql $mysql_flags < /opt/app-root/src/init_db.sql
+if ! mariadb $mysql_falgs -e "SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_NAME = 'clients';" 2>&1 > /dev/null ;
+then
+  mysql $mysql_flags < /opt/app-root/src/init_db.sql
+fi
