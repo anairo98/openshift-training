edd759d503d9d09f8093f58af7f40aee95b0b6ba
christian <christian@christian-ThinkPad.home>
2025-09-29 22:42:12 +0200

    Session6 added db init script feedback

--- session6/build_directories/database/files/init_script.sh
+++ session6/build_directories/database/files/init_script.sh
@@ -1,4 +1,7 @@
 if ! mysql $mysql_flags -e "SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_NAME = 'clients';" 2>&1 > /dev/null ;
 then
+  echo "Initializing clients table"
   mysql $mysql_flags < /opt/app-root/src/init_db.sql
+else
+  echo "clients table already initialized"
 fi
