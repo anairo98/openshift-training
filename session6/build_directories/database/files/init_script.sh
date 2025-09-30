a1172490bcde1987d9a07955031d3da53dfdb468
christian <christian@christian-PC.home>
2025-09-30 09:53:52 +0200

    Corrected session6 db init script

--- session6/build_directories/database/files/init_script.sh
+++ session6/build_directories/database/files/init_script.sh
@@ -1,4 +1,4 @@
-if ! mysql $mysql_flags -e "SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_NAME = 'clients';" 2>&1 > /dev/null ;
+if ! mysql $mysql_flags -e "SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_NAME = 'clients';" | grep "clients" 2>&1 > /dev/null ;
 then
   echo "Initializing clients table"
   mysql $mysql_flags < /opt/app-root/src/init_db.sql
