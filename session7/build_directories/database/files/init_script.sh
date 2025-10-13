5078077d00c7a89db3bef42d9796def67ad33627
christian <christian@christian-PC.home>
2025-10-13 16:48:40 +0200

    Session 7: Corrected error in database init script

--- session7/build_directories/database/files/init_script.sh
+++ session7/build_directories/database/files/init_script.sh
@@ -1,7 +1,7 @@
 if ! mysql $mysql_flags -e "SELECT TABLE_NAME FROM information_schema.TABLES WHERE TABLE_NAME = 'clients';" | grep "clients" 2>&1 > /dev/null ;
 then
   echo "Initializing clients table"
-  echo "USE ${DB_DATABASE};" > /opt/app-root/src/tmp_init_db.sql
+  echo "USE ${MYSQL_DATABASE};" > /opt/app-root/src/tmp_init_db.sql
   cat /opt/app-root/src/init_db >> /opt/app-root/src/tmp_init_db.sql
   mysql $mysql_flags < /opt/app-root/src/tmp_init_db.sql
 else
